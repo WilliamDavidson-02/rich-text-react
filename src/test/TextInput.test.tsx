4ace15f6e3ef3ee0cc183153bdd426daae01f396
William Davidson <wj.davidson@outlook.com>
2023-12-29 15:34:46 +0100

    vitest setup, TextInput on blur validation shows error msg test

---
+++ src/test/TextInput.test.tsx
@@ -0,0 +1,57 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import TextInput from "../components/TextInput";
+import { z } from "zod";
+import { useState } from "react";
+import { formType } from "../pages/Auth";
+import { toast } from "sonner";
+
+const errorMsg = "First name must be least 2 characters";
+
+const Input = () => {
+  const schema = z.string().min(2);
+  const [form, setForm] = useState<formType>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  return (
+    <TextInput
+      text={form.firstName}
+      setText={setForm}
+      placeholder="First name"
+      inputKey="firstName"
+      schema={schema}
+      errorMsg={errorMsg}
+    />
+  );
+};
+
+vi.mock("sonner", () => ({
+  toast: {
+    error: vi.fn(),
+  },
+}));
+
+describe("Text input component", () => {
+  test("insert a valid input", () => {
+    render(<Input />);
+
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "John" } });
+    fireEvent.blur(input); // trigger validation function
+
+    expect(toast.error).not.toHaveBeenCalledWith(errorMsg);
+  });
+
+  test("shows error when input value is less than 2 characters", () => {
+    render(<Input />);
+
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "J" } });
+    fireEvent.blur(input); // trigger validation function
+
+    expect(toast.error).toHaveBeenCalledWith(errorMsg);
+  });
+});
